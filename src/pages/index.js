88e27787c2cc795cb5fde6e827f4ea84350891f5
Olivia Hill <oliviahill99992@gmail.com>
2022-03-31 15:20:51 +0100

    adding ab tasty tag

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,10 +4,17 @@ import Layout from "../components/Layout"
 import { StaticImage } from "gatsby-plugin-image"
 import AllReviews from "../components/AllReviews"
 import Seo from "../components/SEO"
+import { Helmet } from "react-helmet"
 
 export default function Home() {
   return (
     <Layout>
+      <Helmet>
+        <script
+          type="text/javascript"
+          src="https://try.abtasty.com/953e8bf35c69220e883666ed8848025a.js"
+        ></script>
+      </Helmet>
       <Seo title="Home " description="This is the home page" />
       <main className="page">
         <header className="hero">
